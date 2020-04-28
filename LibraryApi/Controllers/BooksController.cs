a4afc7b017074d73fc90dbf077a4665cc8aa663d
mucbro <mucbroski@gmail.com>
2020-04-28 17:55:56 -0400

    pre-swagger check in

---
+++ LibraryApi/Controllers/BooksController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryApi.Domain;
+using LibraryApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace LibraryApi.Controllers
+{
+    public class BooksController : Controller
+    {
+
+        LibraryDataContext Context;
+
+        public BooksController(LibraryDataContext context)
+        {
+            Context = context;
+        }
+
+        [HttpGet("books")]
+        public ActionResult GetAllBooks([FromQuery] string genre)
+        {
+            var books = Context.Books
+                .Where(b => b.InStock)
+                .Select(b => new GetBooksResponseItem
+                {
+                    Id = b.id,
+                    Title = b.Title,
+                    Author = b.Author,
+                    Genre = b.Genre,
+                    NumberOfPages = b.NumberOfPages
+                });
+            
+            if(genre != null)
+            {
+                books = books.Where(b => b.Genre == genre);
+            }
+
+    var booksList = books.ToList();
+            var response = new GetBooksResponse
+            {
+                Books = booksList,
+                GenreFilter = genre,
+                NumberOfBooks = booksList.Count
+            };
+            return Ok(response);
+        }
+
+    }
+}
